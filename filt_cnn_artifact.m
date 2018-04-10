4339cceefc5ebdc8b51df961e403e8b61f1fb0fc
Fujun <luanfj11@gmail.com>
2018-04-09 19:20:51 -0700

    update

--- filt_cnn_artifact.m
+++ filt_cnn_artifact.m
@@ -1,24 +1,26 @@
-% parpool(8)
-% parfor i = 0 : 43
-for i = 2:2 % 70:72
-% parfor i = 0 : 149
+
+for i = 0:34
     close all
+
+    in_fn  = ['results/' int2str(i) '_final_res.png'];
+    out_fn = ['results/' int2str(i) '_final_res2.png'];
     
-     in_fn  = [int2str(i) '_result.jpg'];
-     out_fn = [int2str(i) '_result2.jpg'];
-     
- 
-%     in_fn  = ['../tmp36_wikiart2_current_more/' int2str(i) '_result.png'];
-%     out_fn = ['../tmp36_wikiart2_current_more/' int2str(i) '_result2.png'];
-    
-   
+    if exist(in_fn, 'file') ~= 2 
+        fprintf('file doesn''t exist: %s\n', in_fn); 
+        continue
+    end 
+    if exist(out_fn, 'file') == 2 
+        fprintf('result already exists: %s\n', out_fn); 
+        continue
+    end 
+
     I = im2double(imread(in_fn));
-    
-     G = im2double(imread(['free2use/' int2str(i) '_naive.png']));
-     M = im2double(imread(['free2use/' int2str(i) '_c_mask.png']));
-     B = im2double(imread(['free2use/' int2str(i) '_target.jpg']));
- 
-    
+
+    G = im2double(imread(['data/' int2str(i) '_naive.jpg']));
+    M = im2double(imread(['data/' int2str(i) '_c_mask.jpg']));
+    B = im2double(imread(['data/' int2str(i) '_target.jpg']));
+
+
     tr= 3;
     h = fspecial('gaussian', [2*tr+1 2*tr+1], tr);
     sM = imfilter(M, h, 'same'); 
@@ -26,11 +28,11 @@ for i = 2:2 % 70:72
     sM(sM < 0.01) = 0;
     sM = imfilter(sM, h, 'same'); 
 
-    addpath code/colorspace
+    addpath 3rdparty/colorspace
     I_lab = colorspace('rgb->lab', I);
 
-    addpath code/guided_filter
-    addpath code/patchmatch-2.0
+    addpath 3rdparty/guided_filter
+    addpath 3rdparty/patchmatch-2.0
 
     r = 2; % try r=2, 4, or 8
     eps = 0.1^2; % try eps=0.1^2, 0.2^2, 0.4^2
@@ -47,12 +49,12 @@ for i = 2:2 % 70:72
     patch_w = 7;
     ann = nnmex(O1, B, algo, patch_w, [], [], [], [], [], cores);
     O2_base = im2double(votemex(B, ann));
-    
+
     r = 3;
     h = fspecial('gaussian', [2*r+1 2*r+1], r/3);
     O1_base = imfilter(O1, h, 'same');
     O2 = O2_base + O1 - O1_base;
-    
+
     O2 = O2.*sM + B.*(1-sM);
     figure; imshow(I)
     figure; imshow(O2)
